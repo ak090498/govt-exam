4c95a5cf0e43d6b47c0c97a7219cf75e6b5a18ca
Aritra <aritrakunda3@gmail.com>
2026-08-26 08:24:26 +0530

    fixed form issue

--- stream.py
+++ stream.py
@@ -25,7 +25,8 @@ if st.session_state.clicked:
                      st.write("your exam pdf is uploaded successfully")
                      st.write("Your questions are coming shortly")
            os.remove(file_path)
-counter=0                    
+counter=0
+json_obj=""                    
 with st.form(key="my-input"):
      text_input=st.text_input("Enter your topic")
      submitted=st.form_submit_button("ask")
@@ -38,19 +39,21 @@ with st.form(key="my-input"):
           q_and_a_str=response_str[7:len(response_str)-3]
           json_obj = json.loads(q_and_a_str)
 
+     if json_obj!="":
+          with st.form(key="exam"):
+               for i in range(0,len(json_obj)):
+                    st.write(json_obj[i]['question'])
+                    st.write(json_obj[i]['options'])
+                    answer_input=st.text_input("Enter your answer",key="answer_input"+str(i))
+                    submitted=st.button("answer",key="answer"+str(i))
+                    if submitted:
+                         if answer_input==json_obj[i]['correct_answer']:
+                              st.markdown(":green[correct answer]")
+                              st.write(f"You have answered {counter} questions correctly")
+                              counter+=1
+                         else:
+                              st.markdown(":red[wrong answer]")
 
-          for i in range(0,len(json_obj)):
-               st.write(json_obj[i]['question'])
-               st.write(json_obj[i]['options'])
-               answer_input=st.text_input("Enter your answer",key="answer_input"+str(i))
-               submitted=st.form_submit_button("answer",key="answer"+str(i))
-               if submitted:
-                   if answer_input==json_obj[i]['correct_answer']:
-                      st.markdown(":green[correct answer]")
-                      counter+=1
-                   else:
-                       st.markdown(":red[wrong answer]")
-st.write(f"You have answered {counter} questions correctly")
                # st.write(json_obj[i]['correct_answer'])
           
                   
