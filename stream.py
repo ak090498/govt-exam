d0bd83186a40b9caaab076a34a471480434b6c7a
Aritra <aritrakunda3@gmail.com>
2026-08-26 07:57:03 +0530

    Added question answer format and counter

--- stream.py
+++ stream.py
@@ -24,7 +24,8 @@ if st.session_state.clicked:
                 if response.status_code==200:
                      st.write("your exam pdf is uploaded successfully")
                      st.write("Your questions are coming shortly")
-           os.remove(file_path)          
+           os.remove(file_path)
+counter=0                     
 with st.form(key="my-input"):
      text_input=st.text_input("Enter your topic")
      submitted=st.form_submit_button("ask")
@@ -36,12 +37,22 @@ with st.form(key="my-input"):
           response_str=response_dict['response']['content']
           q_and_a_str=response_str[7:len(response_str)-3]
           json_obj = json.loads(q_and_a_str)
+     
           for i in range(0,len(json_obj)):
                st.write(json_obj[i]['question'])
                st.write(json_obj[i]['options'])
-               st.write(json_obj[i]['correct_answer'])
-
-
+               with st.form(key="answer-key"+str(i)):
+                    answer_input=st.text_input("Enter your answer")
+                    submitted=st.form_submit_button("answer")
+                    if submitted:
+                         if answer_input==json_obj[i]['correct_answer']:
+                              st.markdown(":green[correct answer]")
+                              counter+=1
+                         else:
+                              st.markdown(":red[wrong answer]")
+st.write(f"You have answered {counter} questions correctly")
+               # st.write(json_obj[i]['correct_answer'])
+          
                   
 
 
